3fbae63c427b40d49aa53f143ac21c1ad0a058db
Adir82 <adir.tza2211@gmail.com>
2024-12-15 18:45:26 +0200

    Password

---
+++ Password/Password.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+struct Password
+{
+	char value[16];
+	bool incorrect;
+	Password() : value(""), incorrect(true)
+	{
+	}
+};
+
+int main()
+{
+	std::cout << "Enter your password to continue:" << std::endl;
+	Password pwd;
+	std::cin >> pwd.value;
+
+	if (!strcmp(pwd.value, "********"))
+		pwd.incorrect = false;
+
+	if(!pwd.incorrect)
+		std::cout << "Congratulations\n";
+
+	return 0;
+}
+
+/*
+Working password: ****************
+Reason: since the pwd.value is full it change the behavior of strcmp and makes it return false even when the result should be true
+*/
